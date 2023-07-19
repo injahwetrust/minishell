3bb81396c15b7201c4d20d273eb7631d0e03099c
injahwetrust <bruno.vaujour@gmail.com>
2023-07-19 16:45:26 +0200

    auto

--- exit.c
+++ exit.c
@@ -6,37 +6,85 @@
 /*   By: bvaujour <bvaujour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 12:21:34 by bvaujour          #+#    #+#             */
-/*   Updated: 2023/07/19 13:47:19 by bvaujour         ###   ########.fr       */
+/*   Updated: 2023/07/19 16:44:26 by bvaujour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	too_many_arg(t_data *data, char *cmd)
+static void checkflow(t_data *data, char *cmd, char *max, int i)
 {
-	int	i;
+	long long	ret;
 
-	i = 0;
-	while (cmd[i])
+	max[0] -= 1;
+	cmd[i] -= 1;
+	ret = ft_atoll(cmd);
+	if (ret >= ft_atoll(max))
 	{
-		if (!ft_isdigit(cmd[i]))
-		{
-			printf("exit\n");
-			dprintf(2, "bash: exit: %s: numeric argument required\n", cmd);
-			step0(data);
-			exit(2);
-		}
-		i++;
+		free(max);
+		printf("exit\n");
+		dprintf(2, "Minishell: exit: %s: numeric argument required\n", cmd);
+		step0(data);
+		exit(2);
 	}
+	else
+	{
+		free(max);
+		printf("exit\n");
+		step0(data);
+		exit(ret);
+	}
+}
+
+static void	overflow(t_data *data, char *cmd, char *max)
+{
+		free(max);
+		printf("exit\n");
+		dprintf(2, "bash: exit: %s: numeric argument required\n", cmd);
+		step0(data);
+		exit(2);
+}
+
+static void underflow(t_data *data, char *cmd, char *max)
+{
+	long long ret;
+		
+	free(max);
+	ret = ft_atoll(cmd);
+	step0(data);
 	printf("exit\n");
-	dprintf(2, "Minishell: exit: too many arguments\n");
-	return (1);
+	exit(ret);
+}
+
+static void	check_overflow(t_data *data, char *cmd)
+{
+	char	*max;
+	int		size_cmd;
+	int		size_max;
+	int		i;
+
+	i = -1;
+	max = ft_strdup("9223372036854775808");
+	size_cmd = ft_strlen(cmd);
+	size_max = ft_strlen(max);
+	if (cmd[0] == '-' || cmd[0] == '+')
+	{
+		i++;
+		size_cmd--;
+	}
+	while (cmd[++i] == '0')
+		size_cmd -= 1;
+	if (size_cmd < size_max)
+		underflow(data, cmd, max);
+	else if (size_cmd > size_max)
+		overflow(data, cmd, max);
+	else
+		checkflow(data, cmd, max, i);
 }
 
 static void	parse_exit(t_data *data, char *cmd)
 {
 	int	i;
-	unsigned long long	ret;
 
 	i = -1;
 	while (cmd[++i])
@@ -51,10 +99,30 @@ static void	parse_exit(t_data *data, char *cmd)
 			exit(2);
 		}
 	}
-	ret = ft_atoll(cmd);
-	step0(data);
-	exit (ret);
+	check_overflow(data, cmd);
 }
+
+static int	too_many_arg(t_data *data, char *cmd)
+{
+	int	i;
+
+	i = 0;
+	while (cmd[i])
+	{
+		if (!ft_isdigit(cmd[i]))
+		{
+			printf("exit\n");
+			dprintf(2, "Minishell: exit: %s: numeric argument required\n", cmd);
+			step0(data);
+			exit(2);
+		}
+		i++;
+	}
+	printf("exit\n");
+	dprintf(2, "Minishell: exit: too many arguments\n");
+	return (1);
+}
+
 
 int	manage_exit(t_data *data, char **s_cmd)
 {
